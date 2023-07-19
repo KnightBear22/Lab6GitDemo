e1f3bbe9a8c912e1b3194faa469e25c5c52057c7
KnightBear22 <dehart.bear@gmail.com>
2023-07-19 15:15:04 -0400

    encoder code

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-var_used = 6
+def encode(password):
+    res = ''
+    for digit in password:
+        encoded_digit = str(int(digit) + 3)
+        res += encoded_digit
+    return res
 
-for i in range(1, var_used + 1):
-    for j in range(1, i):
-        print(i)
-        # adding comment to commit and push
-        # testing commit
+
+menu = True
+while menu:
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+    print()
+    option = int(input('Please enter an option: '))
+    if option == 1:
+        password = str(input('Please enter your password to encode: '))
+        encoded_password = encode(password)
+        print('Your password has been encoded and stored!')
+        print()
+    elif option == 2:
+        print(f'The encoded password is {encoded_password}, and the original password is {password}')
+        print()
+    else:
+        menu = False
